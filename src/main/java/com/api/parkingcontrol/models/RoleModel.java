6848bdf075f16e2d9a575a56f58164bdf4a52921
mateusb23 <mateusb.bispo@gmail.com>
2022-12-27 14:53:55 -0300

    Project update

--- src/main/java/com/api/parkingcontrol/models/RoleModel.java
+++ src/main/java/com/api/parkingcontrol/models/RoleModel.java
@@ -1,10 +1,12 @@
 package com.api.parkingcontrol.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.api.parkingcontrol.enums.RoleName;
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "TB_ROLE")
@@ -12,9 +14,24 @@ public class RoleModel implements GrantedAuthority, Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
 
     @Override
     public String getAuthority() {
-        return null;
+        return this.roleName.toString();
     }
+
+    public UUID getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(UUID roleId) {
+        this.roleId = roleId;
+    }
+
 }
